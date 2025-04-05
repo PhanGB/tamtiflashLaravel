ce710521b667a5a9f31cafde79dc96a38388da86
Phuocnc041020 <phuocncps38249@gmail.com>
2025-04-05 22:59:05 +0700

    sửa xíu

--- tamtiflash/resources/views/admin/category_add.blade.php
+++ tamtiflash/resources/views/admin/category_add.blade.php
@@ -10,7 +10,7 @@
                     <div class="bg-light rounded h-100 p-4">
                         <h6 class="mb-4">Thêm danh mục</h6>
                         <a class="btn btn-secondary rounded-pill m-2" href="{{ route('admin.category') }}">Trở lại</a>
-                        <form action="{{ route('addCate') }}" method="post" enctype="multipart/form-data">
+                        <form action="{{ route('admin.addCate') }}" method="post" enctype="multipart/form-data">
                             @csrf
                             <input class="form-control mb-3" type="text" name="name" placeholder="Tên danh mục"
                                 aria-label="default input example">
