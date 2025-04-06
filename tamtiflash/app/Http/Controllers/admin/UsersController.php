e639b2055d23741f9dab413e223be6a1ef933dea
phangb <taolaai2468@gmail.com>
2025-04-06 13:36:36 +0700

    view user

--- tamtiflash/app/Http/Controllers/admin/UsersController.php
+++ tamtiflash/app/Http/Controllers/admin/UsersController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function index()
     {
-
-        return view('admin.users');
+        $users = User::where('role', '=', '0')->get();
+        $data = [
+            'users' => $users,
+        ];
+        return view('admin.users', $data);
     }
 }
